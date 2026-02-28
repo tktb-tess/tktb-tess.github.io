baf6edf8132e1137a07f75c4836388a5f4c0d8a3
tktb-tess <chemusiclove6174@gmail.com>
2026-02-28 17:59:08 +0900

    anchor-link

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,14 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import remarkDirective from 'remark-directive';
 import remarkToc from 'remark-toc';
-import { tableHandler, textDirectiveHandler } from './src/plugins/handlers';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import { fromHtmlIsomorphic } from 'hast-util-from-html-isomorphic';
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
+import {
+  tableHandler,
+  textDirectiveHandler,
+  linkSvg,
+} from './src/plugins/handlers';
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,8 +29,25 @@ export default defineConfig({
     shikiConfig: {
       theme: 'github-dark',
     },
-    remarkPlugins: [remarkMath, remarkDirective, [remarkToc, { heading: '目次' }]],
-    rehypePlugins: [rehypeKatex],
+    remarkPlugins: [
+      remarkMath,
+      remarkDirective,
+      [remarkToc, { heading: '目次' }],
+    ],
+    rehypePlugins: [
+      rehypeKatex,
+      rehypeHeadingIds,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'append',
+          properties: {
+            className: ['anchor-link'],
+          },
+          content: fromHtmlIsomorphic(linkSvg, { fragment: true }).children,
+        },
+      ],
+    ],
     remarkRehype: {
       handlers: {
         table: tableHandler,
