b42aaf558e9b23f57486ccdf2050d558b533f74b
tktb-tess <chemusiclove6174@gmail.com>
2026-03-28 15:02:51 +0900

    a

--- src/plugins/handlers.ts
+++ src/plugins/handlers.ts
@@ -55,7 +55,7 @@ export const textDirectiveHandler: Handler = (_, node: TextDirective) => {
 
 export const tableHandler: Handler = (_, node: Table) => {
   const [head, ...body] = node.children;
-  const ths = head.children.map(
+  const ths = head?.children.map(
     (th): Element => ({
       type: 'element',
       tagName: 'th',
@@ -64,7 +64,7 @@ export const tableHandler: Handler = (_, node: Table) => {
     }),
   );
 
-  const cond = ths.some((th) => th.children.length > 0);
+  const cond = ths != null && ths.some((th) => th.children.length > 0);
 
   const thead: Element | null = cond
     ? {
@@ -120,7 +120,6 @@ export const tableHandler: Handler = (_, node: Table) => {
   };
 };
 
-export const linkSvg = 
-  `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+export const linkSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
     <path d="m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z"></path>
   </svg>`;
